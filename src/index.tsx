a1b63e47d1a5f21e8a0542c1b4ea5353335f4cdd
Yehuda Ringler <yehuda.ringler@chabad.org>
2024-09-11 13:40:55 -0400

    MASTER: Add schedule-x

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,40 @@
-import { render } from 'preact';
-
-import preactLogo from './assets/preact.svg';
-import './style.css';
-
-export function App() {
-	return (
-		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
-	);
+import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/preact'
+import {
+  createViewDay,
+  createViewMonthAgenda,
+  createViewMonthGrid,
+  createViewWeek,
+} from '@schedule-x/calendar'
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+ 
+const dragAndDrop = createDragAndDropPlugin()
+ 
+import '@schedule-x/theme-default/dist/index.css'
+ 
+function CalendarApp() {
+  const calendar = useCalendarApp({
+    views: [
+      createViewDay(),
+      createViewWeek(),
+      createViewMonthGrid(),
+      createViewMonthAgenda(),
+    ],
+    events: [
+      {
+        id: '1',
+        title: 'Event 1',
+        start: '2023-12-16',
+        end: '2023-12-16',
+      },
+    ],
+    plugins: [dragAndDrop],
+  })
+ 
+  return (
+    <div>
+      <ScheduleXCalendar calendarApp={calendar} />
+    </div>
+  )
 }
-
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
-}
-
-render(<App />, document.getElementById('app'));
+ 
+export default CalendarApp;
