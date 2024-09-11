d4ed2db878895edaab0843f2de16dffbfcbb2a3b
Yehuda Ringler <yehuda.ringler@chabad.org>
2024-09-11 14:05:05 -0400

    MASTER: try

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import {
 	createViewMonthAgenda,
 	createViewMonthGrid,
 	createViewWeek,
+	viewDay,
 	viewMonthAgenda,
 	viewMonthGrid,
+	viewWeek,
 } from '@schedule-x/calendar'
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
 
@@ -17,13 +19,13 @@ import { render } from 'preact'
 
 function* getEvents(): Generator<CalendarEvent> {
 
-	for (let start = new Date(), i = 0; i < 20; start.setMinutes(start.getMinutes() + 10), i++) {
+	for (let start = new Date('2023-12-01'), i = 0; i < 20; start.setMinutes(start.getMinutes() + 10), i++) {
 		const end = new Date(start);
 		end.setMinutes(end.getMinutes() + 10);
 
 		yield {
-			start: start.toDateString(),
-			end: end.toDateString(),
+			start: `${start.getFullYear()}-${start.getMonth()}-${start.getDate()} ${start.toTimeString().split(' ')[0]}`,
+			end: `${end.getFullYear()}-${end.getMonth()}-${end.getDate()} ${end.toTimeString().split(' ')[0]}`,
 			id: i.toString(),
 			title: `Event ${i}`
 		}
@@ -34,15 +36,25 @@ function CalendarApp() {
 
 
 	const calendar = useCalendarApp({
-		views: [
-			createViewDay(),
-			createViewWeek(),
-			createViewMonthGrid(),
-			createViewMonthAgenda(),
-		],
-		defaultView: viewMonthAgenda.name,
-		events: [...getEvents()],
-		plugins: [dragAndDrop],
+		views: [viewMonthGrid, viewMonthAgenda, viewWeek, viewDay],
+		selectedDate: '2023-12-01',
+		defaultView: viewWeek.name,
+		events: Array.from(getEvents()),
+		calendars: {
+		  leisure: {
+			colorName: 'leisure',
+			lightColors: {
+			  main: '#1c7df9',
+			  container: '#d2e7ff',
+			  onContainer: '#002859',
+			},
+			darkColors: {
+			  main: '#c0dfff',
+			  onContainer: '#dee6ff',
+			  container: '#426aa2',
+			},
+		  },
+		},
 	})
 
 	return (
