3bddbcc19767984dac7a0a49136e2604013d9321
Yehuda Ringler <yehuda.ringler@chabad.org>
2024-09-11 13:58:02 -0400

    MASTER: Try add event

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,58 @@
 import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/preact'
 import {
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
+	CalendarEvent,
+	createViewDay,
+	createViewMonthAgenda,
+	createViewMonthGrid,
+	createViewWeek,
+	viewMonthAgenda,
+	viewMonthGrid,
 } from '@schedule-x/calendar'
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
- 
+
 const dragAndDrop = createDragAndDropPlugin()
- 
+
 import '@schedule-x/theme-default/dist/index.css'
- 
+import { render } from 'preact'
+
+function* getEvents(): Generator<CalendarEvent> {
+
+	for (let start = new Date(), i = 0; i < 20; start.setMinutes(start.getMinutes() + 10), i++) {
+		const end = new Date(start);
+		end.setMinutes(end.getMinutes() + 10);
+
+		yield {
+			start: start.toDateString(),
+			end: end.toDateString(),
+			id: i.toString(),
+			title: `Event ${i}`
+		}
+	}
+};
+
 function CalendarApp() {
-  const calendar = useCalendarApp({
-    views: [
-      createViewDay(),
-      createViewWeek(),
-      createViewMonthGrid(),
-      createViewMonthAgenda(),
-    ],
-    events: [
-      {
-        id: '1',
-        title: 'Event 1',
-        start: '2023-12-16',
-        end: '2023-12-16',
-      },
-    ],
-    plugins: [dragAndDrop],
-  })
- 
-  return (
-    <div>
-      <ScheduleXCalendar calendarApp={calendar} />
-    </div>
-  )
+
+
+	const calendar = useCalendarApp({
+		views: [
+			createViewDay(),
+			createViewWeek(),
+			createViewMonthGrid(),
+			createViewMonthAgenda(),
+		],
+		defaultView: viewMonthAgenda.name,
+		events: [...getEvents()],
+		plugins: [dragAndDrop],
+	})
+
+	return (
+		<div>
+			<ScheduleXCalendar calendarApp={calendar} />
+		</div>
+	)
 }
- 
+
+render(<CalendarApp />, document.getElementById('app'));
+
+
 export default CalendarApp;
